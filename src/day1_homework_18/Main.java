89945f06a7ed41885af04781d76392b9d2f593d9
Dursun Köktürk <dursunserefkokturk@gmail.com>
2022-09-08 12:57:44 +0300

    Friend Number Control

---
+++ src/day1_homework_18/Main.java
@@ -0,0 +1,35 @@
+package day1_homework_18;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Arkadas Sayi
+		// Girilen Iki Farkli Sayidan Birinin 
+		// Kendisi Haric Kendisine Kadar Olan Sayilarin 
+		// Tam Bolenlerinin Toplami Diger Gilen Sayiyi Vermesidir
+		int number1 = 220;
+		int number2 = 284;
+		
+		int total1 = 0;
+		int total2 = 0;
+		
+		for (int i = 1 ; i < number1 ; i++) {
+			if (number1 % i == 0) {
+				total1 = total1 + i;
+			}
+		}
+		
+		for (int i = 1 ; i < number2 ; i++) {
+			if (number2 % i == 0) {
+				total2 = total2 + i;
+			}
+		}
+		
+		if (number1 == total2 && number2 == total1) {
+			System.out.println("Girilen Sayýlar Arkadaþ Sayýdýr");
+		}else {
+			System.out.println("Girilen Sayýlar Arkadaþ Sayý Deðildir");
+		}
+	}
+}
